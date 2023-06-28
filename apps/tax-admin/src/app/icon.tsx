5a4394c0d96340fbeaf672bd0284c26a70e8dac6
Florian Gudat <florian.gudat.1@stud.htwk-leipzig.de>
2023-06-28 08:57:13 +0000

    feat: tax office admin infrastructure

---
+++ apps/tax-admin/src/app/icon.tsx
@@ -0,0 +1,41 @@
+import { ImageResponse } from "next/server";
+
+// Route segment config
+export const runtime = "edge";
+
+// Image metadata
+export const size = {
+  width: 55,
+  height: 55,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 40,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          borderRadius: 10,
+        }}
+      >
+          🐹
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
---
+++ apps/tax-admin/src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Empty } from "antd";
+
+export default function Page() {
+  return (
+    <Empty description={"There is no content yet!"} style={{ marginTop: 50 }} />
+  );
+}
