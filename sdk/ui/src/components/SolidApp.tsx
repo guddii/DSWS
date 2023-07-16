d8c2753afb44044e73c69af84daf8a5bf07a0539
Florian Gudat <florian.gudat@guices.com>
2023-07-17 00:47:55 +0200

    chore: split layout into separate components

--- sdk/ui/src/components/SolidApp.tsx
+++ sdk/ui/src/components/SolidApp.tsx
@@ -1,18 +1,20 @@
-import { ReactNode } from "react";
-import { Col, Layout, Row, Space, Typography } from "antd";
-import { SessionProvider } from "@inrupt/solid-ui-react";
-import { ControlsSession } from "./controls/ControlsSession";
+import React, { ReactNode } from "react";
+import { Layout } from "antd";
 import { Metadata } from "next";
-
-const { Header, Content, Footer } = Layout;
-const { Title } = Typography;
+import { SessionProvider } from "@inrupt/solid-ui-react";
+import { LayoutMasthead } from "./layout/LayoutMasthead";
+import { LayoutContent } from "./layout/LayoutContent";
+import { LayoutFooter } from "./layout/LayoutFooter";
 
 interface ISolidAppProperties {
   children: ReactNode;
   metadata: Metadata;
 }
 
-export const SolidApp: React.FC<any> = ({ children, metadata }) => {
+export const SolidApp: React.FC<any> = ({
+  children,
+  metadata,
+}: ISolidAppProperties) => {
   return (
     <SessionProvider
       sessionId="session-provider-example"
@@ -20,37 +22,9 @@ export const SolidApp: React.FC<any> = ({ children, metadata }) => {
       restorePreviousSession={false}
     >
       <Layout style={{ minHeight: "100vh" }}>
-        <Layout className="site-layout">
-          <Header style={{ background: "transparent", padding: "0 16px" }}>
-            <Row>
-              <Col span={6}>
-                <div style={{ width: "100%", textOverflow: "ellipsis" }}>
-                  Solid Showcase
-                  <Title style={{ marginTop: "-16px" }}>
-                    <Space>
-                      <img
-                        src={"/icon"}
-                        alt="App brand"
-                        width={55}
-                        height={55}
-                      />
-                      <div style={{ width: 320 }}> {metadata.title} </div>
-                    </Space>
-                  </Title>
-                </div>
-              </Col>
-              <Col span={18} style={{ textAlign: "right" }}>
-                <ControlsSession metadata={metadata} />
-              </Col>
-            </Row>
-          </Header>
-          <Content style={{ margin: "32px 16px 16px 16px" }}>
-            {children}
-          </Content>
-          <Footer style={{ textAlign: "center" }}>
-            ©{new Date().getFullYear()}
-          </Footer>
-        </Layout>
+        <LayoutMasthead metadata={metadata} />
+        <LayoutContent> {children} </LayoutContent>
+        <LayoutFooter />
       </Layout>
     </SessionProvider>
   );
---
+++ sdk/ui/src/components/layout/LayoutMasthead.tsx
@@ -0,0 +1,30 @@
+import { Col, Layout, Row, Space, Typography } from "antd";
+import { ControlsSession } from "../controls/ControlsSession";
+import { Metadata } from "next";
+
+interface ILayoutMastheadProperties {
+  metadata: Metadata;
+}
+
+export function LayoutMasthead({ metadata }: ILayoutMastheadProperties) {
+  return (
+    <Layout.Header style={{ background: "transparent", padding: "0 16px" }}>
+      <Row>
+        <Col span={6}>
+          <div style={{ width: "100%", textOverflow: "ellipsis" }}>
+            Solid Showcase
+            <Typography.Title style={{ marginTop: "-16px" }}>
+              <Space>
+                <img src={"/icon"} alt="App brand" width={55} height={55} />
+                <div style={{ width: 320 }}> {String(metadata.title)} </div>
+              </Space>
+            </Typography.Title>
+          </div>
+        </Col>
+        <Col span={18} style={{ textAlign: "right" }}>
+          <ControlsSession metadata={metadata} />
+        </Col>
+      </Row>
+    </Layout.Header>
+  );
+}
