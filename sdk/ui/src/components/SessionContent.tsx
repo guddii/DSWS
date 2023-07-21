968f770ffc0b7d8c7fdc077c2667c2189ba02ad4
Florian Gudat <florian.gudat@guices.com>
2023-07-21 19:32:40 +0200

    refactor: removed webId and storage loader

--- sdk/ui/src/components/SessionContent.tsx
+++ sdk/ui/src/components/SessionContent.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { ReactNode } from "react";
-import { Empty } from "antd";
+import React, { CSSProperties, ReactNode } from "react";
+import { Button, Empty, Space, Typography } from "antd";
 import { useIdentity } from "../contexts/IdentityContext";
-import { WebIdLoader } from "./loader/WebIdLoader";
+import { ControlsLogin } from "./controls/ControlsLogin";
+import { DatabaseOutlined } from "@ant-design/icons";
+import Link from "next/link";
 
 interface ILoggedInContentProperties {
   children: ReactNode;
@@ -15,18 +17,45 @@ export const SessionContent = ({
 }: ILoggedInContentProperties) => {
   const { webId } = useIdentity();
 
-  if (!webId) {
-    if (alwaysShowChildren) {
-      return <>{children}</>;
-    } else {
-      return (
-        <Empty
-          description={"This is private content, please login first!"}
-          style={{ marginTop: 50 }}
-        />
-      );
-    }
+  if (!alwaysShowChildren && !webId) {
+    const style: CSSProperties = {
+      width: 350,
+      marginLeft: "auto",
+      marginRight: "auto",
+    };
+    return (
+      <Empty
+        style={{ marginTop: 50 }}
+        description={
+          <div>
+            <Typography.Paragraph style={style}>
+              This application requires a Pod as data storage and a WebId to
+              identify yourself in the web.
+            </Typography.Paragraph>
+            <Typography.Paragraph style={style}>
+              Find out more at{" "}
+              <Link href="https://solidproject.org/" target={"_blank"}>
+                https://solidproject.org/
+              </Link>
+              .
+            </Typography.Paragraph>
+          </div>
+        }
+      >
+        <Space>
+          <ControlsLogin />
+          <Button
+            type="primary"
+            icon={<DatabaseOutlined rev={"solidGetAPod"} />}
+            href={"https://solidproject.org/users/get-a-pod"}
+            target={"_blank"}
+          >
+            Get a Pod
+          </Button>
+        </Space>
+      </Empty>
+    );
   }
 
-  return <WebIdLoader>{children}</WebIdLoader>;
+  return <>{children}</>;
 };
