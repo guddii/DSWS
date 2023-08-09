c33df6a7171be42e6bb774e5f1c737235f8a6553
Florian Gudat <florian.gudat@guices.com>
2023-08-10 01:02:10 +0200

    fix: blocked webId modal

--- sdk/ui/src/components/formItem/FormItemWebId.tsx
+++ sdk/ui/src/components/formItem/FormItemWebId.tsx
@@ -1,10 +1,15 @@
 import { Form, Input, FormItemProps } from "antd";
+import { useIdentity } from "../../contexts/IdentityContext";
 
 interface IFormItemWebIdProperties {
   formItemProps?: FormItemProps;
 }
 
 export function FormItemWebId({ formItemProps }: IFormItemWebIdProperties) {
+  const { webId } = useIdentity();
+  const form = Form.useFormInstance();
+  form.setFieldsValue({ webId });
+
   return (
     <Form.Item
       hidden={true}
