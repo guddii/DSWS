b4b81b7464a005d7c5dfc59313f84b0c838e4996
Sergi Domenech Guzy <sergi.domenech_guzy@stud.htwk-leipzig.de>
2023-08-09 19:29:45 +0000

    feat(citizen): append inbox data to stammdaten.ttl

--- sdk/solid/src/config.ts
+++ sdk/solid/src/config.ts
@@ -1,3 +1,5 @@
+import { UrlString } from "@inrupt/solid-client";
+
 export const STAMMDATEN_FOLDER_NAME = "stammdaten";
 export const STAMMDATEN_FOLDER_PATH = `${STAMMDATEN_FOLDER_NAME}/`;
 
@@ -6,3 +8,10 @@ export const STAMMDATEN_FILE_PATH = `${STAMMDATEN_FOLDER_NAME}/${STAMMDATEN_FILE
 
 export const INBOX_FOLDER_NAME = "inbox";
 export const INBOX_FOLDER_PATH = `${INBOX_FOLDER_NAME}/`;
+
+export const SENDER_TO_PROPERTY_MAP: Record<UrlString, UrlString> = {
+  "https://id.inrupt.com/taxofficeapp":
+    "http://custom-predicates.org/hasTaxData",
+  "https://id.inrupt.com/landregistryofficeapp":
+    "http://custom-predicates.org/hasLandRegistryData",
+};
--- sdk/ui/src/components/inbox/InboxMessageCardSaveButton.tsx
+++ sdk/ui/src/components/inbox/InboxMessageCardSaveButton.tsx
@@ -1,23 +1,87 @@
 import { Button, message } from "antd";
-import { getUrl, schema } from "solid";
+import {
+  SENDER_TO_PROPERTY_MAP,
+  STAMMDATEN_FILE_PATH,
+  UrlString,
+  buildThing,
+  createUrl,
+  getSolidDataset,
+  getThing,
+  getUrl,
+  saveSolidDatasetAt,
+  schema,
+  setThing,
+  toUrlString,
+} from "solid";
 import { InboxContent, InboxMessage } from "./InboxMessageCard";
+import { useSession } from "@inrupt/solid-ui-react";
+import { useIdentity } from "../../contexts/IdentityContext";
 
 interface IInboxMessageCardSaveButtonProperties {
   inboxMessage?: InboxMessage;
   inboxContent?: InboxContent;
   disabled?: boolean;
+  onSuccess: () => void;
 }
 
 export const InboxMessageCardSaveButton = ({
   inboxMessage,
   inboxContent,
   disabled,
+  onSuccess,
 }: IInboxMessageCardSaveButtonProperties) => {
+  const { session } = useSession();
+  const { storage } = useIdentity();
+
+  const onClick = async () => {
+    try {
+      if (!inboxMessage || !inboxContent) {
+        throw new Error();
+      }
+
+      const sender = getUrl(inboxMessage, schema.sender);
+      const referenceUrl = getUrl(inboxContent, schema.subjectOf);
+      if (
+        !sender ||
+        !referenceUrl ||
+        !SENDER_TO_PROPERTY_MAP.hasOwnProperty(sender)
+      ) {
+        throw new Error();
+      }
+      const predicate: UrlString = SENDER_TO_PROPERTY_MAP[sender];
+
+      const datasetUrl = toUrlString(createUrl(STAMMDATEN_FILE_PATH, storage));
+      const dataset = await getSolidDataset(datasetUrl, {
+        fetch: session.fetch,
+      });
+
+      let thing = getThing(dataset, `${datasetUrl}#me`);
+      if (!thing) {
+        throw new Error();
+      }
+
+      const updatedThing = buildThing(thing)
+        .addUrl(predicate, referenceUrl)
+        .build();
+      const updatedDataset = setThing(dataset, updatedThing);
+
+      await saveSolidDatasetAt(datasetUrl, updatedDataset, {
+        fetch: session.fetch,
+      });
+
+      message.success("Successfully saved message data to stammdaten.");
+      onSuccess();
+    } catch (error: any) {
+      message.error(
+        error.message || "Some necessary data is missing in message."
+      );
+      console.error(error);
+    }
+  };
+
   return (
     <Button
-      onClick={() =>
-        message.info("Save to data functionality not yet implemented")
-      }
+      onClick={onClick}
       disabled={
         !inboxMessage ||
         !inboxContent ||
