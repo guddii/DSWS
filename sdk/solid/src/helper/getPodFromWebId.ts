39d3fc57d387b95a4c08174d19dc05d1933b8c1b
Florian Gudat <florian.gudat@guices.com>
2023-09-02 14:01:29 +0200

    refactor: reduce code duplications for pod retrieval

--- sdk/solid/src/helper/getPodFromWebId.ts
+++ sdk/solid/src/helper/getPodFromWebId.ts
@@ -1,29 +1,15 @@
-import {
-  UrlString,
-  WebId,
-  getSolidDataset,
-  getThing,
-  getUrl,
-} from "@inrupt/solid-client";
+import { UrlString, WebId, getPodUrlAll } from "@inrupt/solid-client";
 import { Session } from "@inrupt/solid-client-authn-node";
-import { WS } from "@inrupt/vocab-solid";
 
 /**
  * Retrieves the main storage pod for the provided webId.
  * @param session logged in solid session
  * @param webId webId to get the corresponding storage pod
- * @returns url to storage pod or null
+ * @returns url to storage pod or undefined
  */
 export const getPodFromWebId = async (
   session: Session,
   webId: WebId
-): Promise<UrlString | null> => {
-  const dataset = await getSolidDataset(webId, { fetch: session.fetch });
-  const thing = getThing(dataset, webId);
-
-  if (!thing) {
-    return null;
-  }
-
-  return getUrl(thing, WS.storage);
+): Promise<UrlString | undefined> => {
+  return (await getPodUrlAll(webId, { fetch: session.fetch }))[0];
 };
