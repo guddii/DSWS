4b341f88a667e79abbc32892e12776c0c43b1e51
Florian Gudat <florian.gudat@guices.com>
2023-09-17 10:21:36 +0200

    fix: unique app icons

--- apps/registration-office/src/app/icon.tsx
+++ apps/registration-office/src/app/icon.tsx
@@ -28,7 +28,7 @@ export default function Icon() {
           borderRadius: 10,
         }}
       >
-        🐵
+        🐹
       </div>
     ),
     // ImageResponse options
--- apps/vehicle-registration-office/src/app/icon.tsx
+++ apps/vehicle-registration-office/src/app/icon.tsx
@@ -28,7 +28,7 @@ export default function Icon() {
           borderRadius: 10,
         }}
       >
-        🐻
+        🐨
       </div>
     ),
     // ImageResponse options
